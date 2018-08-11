e8192603d0b9488e9777328a1d0f1a26ae3d4205
elraphty <elraphtyofficial@gmail.com>
2018-08-11 18:13:57 +0100

    heroku changes

--- models/mongoose.js
+++ models/mongoose.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 
 // Using Global Promise for Mongoose
 mongoose.Promise = global.Promise;
-
+/*
 mongoose.connect('mongodb://localhost:27017/videoIdea',{
+    useNewUrlParser: true
+})*/
+mongoose.connect('mongodb://elraphty:Elraphty1@ds139949.mlab.com:39949/videoideas',{
     useNewUrlParser: true
 })
  .then(() => { console.log("MongoDb connected") })
